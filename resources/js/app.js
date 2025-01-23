e229fd236e65c3750254295306d4cad73bd1ee62
selvakumar <selvakumar.owc@gmail.com>
2025-01-23 22:02:48 +0530

    laravel version 10 update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,1 @@
 import './bootstrap';
-
-import Alpine from 'alpinejs';
-import 'bootstrap';
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import $ from 'jquery';
-import 'toastr/build/toastr.min.css';
-import toastr from 'toastr';  
-window.$ = $; 
-
-window.Alpine = Alpine;
-
-Alpine.start();
